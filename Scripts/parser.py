67ba6c778c05fa49ea2f44ca7d109161d10670e6
GeraMantecon <lateralus_reflection@hotmail.com>
2017-12-20 20:01:11 -0600

    Fix for different ham and spam

--- Scripts/parser.py
+++ Scripts/parser.py
@@ -12,10 +12,21 @@ def get_body(payload):
     return body
 
 def parse_raw_spam():
-    for root, dirs, files in os.walk('../Resources'):
+    for root, dirs, files in os.walk('../Resources/enron6/spam'):
         with open("spam.txt", "w") as output_file:
             for file in files:
-                with open('../Resources/' + file) as email_file:
+                with open('../Resources/enron6/spam/' + file) as email_file:
+                    email_object = email.message_from_file(email_file)
+                    output = {}
+                    output["subject"] = filter(lambda x: x in set(string.printable), str(email_object["Subject"]).replace('\r\n',' '))
+                    output["body"] = filter(lambda x: x in set(string.printable), get_body(email_object).replace('\r\n',' '))
+                    output_file.write(output.__str__())
+                    output_file.write("\n")
+def parse_raw_ham():
+    for root, dirs, files in os.walk('../Resources/enron6/ham'):
+        with open("ham.txt", "w") as output_file:
+            for file in files:
+                with open('../Resources/enron6/ham/' + file) as email_file:
                     email_object = email.message_from_file(email_file)
                     output = {}
                     output["subject"] = filter(lambda x: x in set(string.printable), str(email_object["Subject"]).replace('\r\n',' '))
--- Scripts/spam_project.py
+++ Scripts/spam_project.py
@@ -9,7 +9,9 @@ import argparse
 import nltk
 import gensim
 import numpy as np
+import pandas as pd
 from parser import parse_raw_spam
+from parser import parse_raw_ham
 from plot import plot_clusters
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
@@ -73,7 +75,7 @@ def get_continuous_chunks(text):
 			else:
 				continue
 	return continuous_chunk
-def generate_features(spam_messages, feature, english_only=False,):
+def generate_features(spam_messages, feature, english_only=False):
 	if feature == 'POS':
 		spam_features = [pos_tag(word_tokenize(spam)) for spam in spam_messages]
 		if english_only:
@@ -98,75 +100,96 @@ def generate_features(spam_messages, feature, english_only=False,):
 	else:
 		spam_features = spam_messages
 	return spam_features
-def train_models(spam_messages, spam_features,	feature, k):
-	km = KMeans(n_clusters=k, init='k-means++', max_iter=100, n_init=1, verbose=0)
-	if feature == 'BOW':
-		vectorizer = TfidfVectorizer(use_idf=True, ngram_range=(1,1),lowercase=True)
-		tfidfs = vectorizer.fit_transform(spam_messages)
-		results = km.fit_transform(tfidfs)
-	elif feature == 'BIGRAMS':
-		vectorizer = TfidfVectorizer(use_idf=True, ngram_range=(2,2),lowercase=True)
-		tfidfs = vectorizer.fit_transform(spam_messages)
-		results = km.fit_transform(tfidfs)
-	elif feature == 'TRIGRAMS':
-		vectorizer = TfidfVectorizer(use_idf=True, ngram_range=(3,3),lowercase=True)
-		tfidfs = vectorizer.fit_transform(spam_messages)
-		results = km.fit_transform(tfidfs)
-	elif feature == 'W2V':
-		vectorizer = TfidfEmbeddingVectorizer(spam_features)
-		tfidfs = vectorizer.fit(spam_messages).transform(spam_messages)
-		results = km.fit_transform(tfidfs)
-	elif feature == 'D2V':
-		spam_vectors = spam_features.wv.syn0
-		results = km.fit_transform(spam_vectors)
-	elif feature == 'NAMED_ENTITIES':
-		vocabulary = [token for spam in spam_features for token in word_tokenize(spam)]
-		vocabulary = list(set(vocabulary))
-		vectorizer = TfidfVectorizer(use_idf=True, vocabulary = vocabulary, tokenizer=lambda i:i, lowercase=False)
-		tfidfs = vectorizer.fit_transform(spam_messages)
-		results = km.fit_transform(tfidfs)
-	elif feature == 'POS':
-		vectorizer = TfidfVectorizer(use_idf=True, ngram_range=(1,1), lowercase=True)
-		tfidfs = vectorizer.fit_transform(spam_messages)
-		results = km.fit_transform(tfidfs)
-	else:
-		print 'ERROR: FEATURE NOT SUPPORTED.'
-		results = []
-		labels = []
-	return results,km.labels_
-def pre_process(stopword=False,punctuation=False,lematize=False):
-	with open('../Resources/spam.txt') as f:
+def print_labeled_tfidfs(tfidfs,messages,vectorizer,labels,k,feature,spam=False):
+    tfidf = pd.DataFrame(tfidfs.toarray(),index=[index for index, value in enumerate(messages)],columns=vectorizer.get_feature_names())
+    if k >= 0:
+        new_df = pd.DataFrame(data=labels, index=[index for index, value in enumerate(labels)], columns=['Label'])
+    else:
+        new_df = pd.DataFrame(data=[-1 for label in labels], index=[index for index, value in enumerate(labels)], columns=['Label'])
+    final_df = pd.merge(tfidf,new_df,left_index=True,right_index=True)
+    if spam:
+        final_df.to_csv('../Results/Spam'+feature+'_'+str(k)+'.csv',index=False)
+    else:
+        final_df.to_csv('../Results/Ham'+feature+'_'+str(k)+'.csv',index=False)
+def train_models_clustering(spam_messages,spam_features,feature,k):
+    km = KMeans(n_clusters=k, init='k-means++', max_iter=100, n_init=1, verbose=0)
+    if feature == 'BOW':
+        vectorizer = TfidfVectorizer(use_idf=True, ngram_range=(1,1),lowercase=True)
+        tfidfs = vectorizer.fit_transform(spam_messages)
+        results = km.fit_transform(tfidfs)
+    elif feature == 'BIGRAMS':
+        vectorizer = TfidfVectorizer(use_idf=True, ngram_range=(2,2),lowercase=True)
+        tfidfs = vectorizer.fit_transform(spam_messages)
+        results = km.fit_transform(tfidfs)
+        #print_labeled_tfidfs(tfidfs,spam_messages,vectorizer,km.labels_,k,feature,True)
+    elif feature == 'TRIGRAMS':
+        vectorizer = TfidfVectorizer(use_idf=True, ngram_range=(3,3),lowercase=True)
+        tfidfs = vectorizer.fit_transform(spam_messages)
+        results = km.fit_transform(tfidfs)
+        #print_labeled_tfidfs(tfidfs,spam_messages,vectorizer,km.labels_,k,feature,True)
+    elif feature == 'W2V':
+        vectorizer = TfidfEmbeddingVectorizer(spam_features)
+        tfidfs = vectorizer.fit(spam_messages).transform(spam_messages)
+        results = km.fit_transform(tfidfs)
+        #print_labeled_tfidfs(tfidfs,spam_messages,vectorizer,km.labels_,k,feature,True)
+    elif feature == 'D2V':
+        spam_vectors = spam_features.wv.syn0
+        results = km.fit_transform(spam_vectors)
+        #print_labeled_tfidfs(tfidfs,spam_messages,vectorizer,km.labels_,k,feature,True)
+    elif feature == 'NAMED_ENTITIES':
+        vocabulary = [token for spam in spam_features for token in word_tokenize(spam)]
+        vocabulary = list(set(vocabulary))
+        vectorizer = TfidfVectorizer(use_idf=True, vocabulary = vocabulary, tokenizer=lambda i:i, lowercase=False)
+        tfidfs = vectorizer.fit_transform(spam_messages)
+        results = km.fit_transform(tfidfs)
+    elif feature == 'POS':
+        vectorizer = TfidfVectorizer(use_idf=True, ngram_range=(1,1), lowercase=True)
+        tfidfs = vectorizer.fit_transform(spam_messages)
+        results = km.fit_transform(tfidfs)
+    else:
+        print 'ERROR: FEATURE NOT SUPPORTED.'
+        results = []
+        labels = []
+    return results,km.labels_
+def pre_process(stopword=False,punctuation=False,lematize=False,isSpam=False):
+    if isSpam:
+        path='../Resources/spam.txt'
+    else:
+        path='../Resources/ham.txt'
+    print path
+    with open(path) as f:
 		spams = f.readlines()
-	spams = [eval(spam)['body'] for spam in spams]
-	if punctuation:
-		spams = [" ".join([re.sub(r'[^\w]', '', token) for token in spam.lower().strip().split() if token not in set(string.punctuation)]) for spam in spams]
-	if stopwords:
-		spams = [" ".join([token for token in spam.lower().strip().split() if token not in set(stopwords.words("english"))]) for spam in spams]
-	if lematize:
-		tk = LemmaTokenizer()
-		spams = [" ".join(tk(spam.lower().strip())) for spam in spams]
-	return spams
+    spams = [eval(spam)['body'] for spam in spams]
+    if punctuation:
+        spams = [" ".join([re.sub(r'[^\w]', '', token) for token in spam.lower().strip().split() if token not in set(string.punctuation)]) for spam in spams]
+    if stopwords:
+        spams = [" ".join([token for token in spam.lower().strip().split() if token not in set(stopwords.words("english"))]) for spam in spams]
+    if lematize:
+        tk = LemmaTokenizer()
+        spams = [" ".join(tk(spam.lower().strip())) for spam in spams]
+    return spams
 def main(argv):
-	parser = argparse.ArgumentParser(description='SCV Spam Classification Viaduct')
-	parser.add_argument('FEATURE', type=str, help='Desired feature upon which the clustering algorithm is goin to be trained: POS, BOW, BIGRAMS, TRIGRAMS, W2V, NAMED_ENTITIES')
-	parser.add_argument('k', type=int ,help='K number for K-mean')
-	parser.add_argument('-eo','--english', action ='store_true', help='Filter to work only with english.')
-	parser.add_argument('-sw','--stopword', action ='store_true', help='Remove stopwords.')
-	parser.add_argument('-p','--punctuation', action ='store_true', help='Remove punctuation marks.')
-	parser.add_argument('-l', '--lematize', action ='store_true',help='Lemmatize tokens.')
-	parser.add_argument('-m', '--models', action ='store_true',help='Use already trained model on Models directory.')
-	args = parser.parse_args()
-	if args.FEATURE not in ['POS','BOW','BIGRAMS','TRIGRAMS','W2V','NAMED_ENTITIES','D2V']:
-		print 'Feature not recognized by program.'
-		sys.exit()
-	if not os.path.isfile('../Resources/spam.txt'):
-		parse_raw_spam()
-	spam_messages = pre_process(args.stopword, args.punctuation, args.lematize)
-	spam_features = generate_features(spam_messages,args.FEATURE,args.english)
-	results,labels = train_models(spam_messages,spam_features,args.FEATURE,args.k)
-	score = metrics.silhouette_score(results, labels, metric='euclidean')
-	plot_clusters(results,args.k,labels,args.FEATURE)
-	print 'K-means with '+ str(args.k) +' clusters using '+ str(args.FEATURE) +' silhouette score: ' + str(score)
-
+    parser = argparse.ArgumentParser(description='SCV Spam Classification Viaduct')
+    parser.add_argument('FEATURE', type=str, help='Desired feature upon which the clustering algorithm is goin to be trained: POS, BOW, BIGRAMS, TRIGRAMS, W2V, NAMED_ENTITIES')
+    parser.add_argument('k', type=int ,help='K number for K-mean')
+    parser.add_argument('-eo','--english', action ='store_true', help='Filter to work only with english.')
+    parser.add_argument('-sw','--stopword', action ='store_true', help='Remove stopwords.')
+    parser.add_argument('-p','--punctuation', action ='store_true', help='Remove punctuation marks.')
+    parser.add_argument('-l', '--lematize', action ='store_true',help='Lemmatize tokens.')
+    parser.add_argument('-m', '--models', action ='store_true',help='Use already trained model on Models directory.')
+    args = parser.parse_args()
+    if args.FEATURE not in ['POS','BOW','BIGRAMS','TRIGRAMS','W2V','NAMED_ENTITIES','D2V']:
+        print 'Feature not recognized by program.'
+        sys.exit()
+    if not os.path.isfile('../Resources/spam.txt'):
+        parse_raw_spam()
+    if not os.path.isfile('../Resources/ham.txt'):
+        parse_raw_ham()
+    spam_messages = pre_process(args.stopword, args.punctuation, args.lematize,isSpam=True)
+    spam_features = generate_features(spam_messages,args.FEATURE,args.english)
+    results,labels = train_models_clustering(spam_messages,spam_features,args.FEATURE,args.k)
+    score = metrics.silhouette_score(results, labels, metric='euclidean')
+    plot_clusters(results,args.k,labels,args.FEATURE)
+    print 'K-means with '+ str(args.k) +' clusters using '+ str(args.FEATURE) +' silhouette score: ' + str(score)
 if __name__ == '__main__':
     main(sys.argv[1:])
